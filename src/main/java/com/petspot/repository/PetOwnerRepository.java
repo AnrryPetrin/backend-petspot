730529c44bf3009e99aa4b4d7bff5a2dfefd7f71
Anrry Petrin De Araujo <108158140+AnrryPetrin@users.noreply.github.com>
2024-10-16 23:23:49 -0300

    Refactor PetOwnerRepository to update field types and use UUID for pet identification

--- src/main/java/com/petspot/repository/PetOwnerRepository.java
+++ src/main/java/com/petspot/repository/PetOwnerRepository.java
@@ -3,5 +3,7 @@ package com.petspot.repository;
 import com.petspot.model.PetOwner;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PetOwnerRepository extends JpaRepository<PetOwner, String> {
+import java.util.UUID;
+
+public interface PetOwnerRepository extends JpaRepository<PetOwner, UUID> {
 }
