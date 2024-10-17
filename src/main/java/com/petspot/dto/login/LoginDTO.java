d88d007958cc37fc918d09aeabd59ce04c2116c9
Anrry Petrin De Araujo <108158140+AnrryPetrin@users.noreply.github.com>
2024-10-16 23:23:08 -0300

    Refactor LoginDTO to update field names and add validation for email and password

--- src/main/java/com/petspot/dto/login/LoginDTO.java
+++ src/main/java/com/petspot/dto/login/LoginDTO.java
@@ -1,8 +1,9 @@
 package com.petspot.dto.login;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 
 public record LoginDTO(
-                @Email String email,
-                String senha) {
+        @Email @NotBlank(message = "Email cannot be blank") String email,
+        @NotBlank(message = "Password cannot be blank") String password) {
 }
