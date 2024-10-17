a02d6ada886fb2d0caa749f3bf6632ff913405cf
Anrry Petrin De Araujo <108158140+AnrryPetrin@users.noreply.github.com>
2024-10-16 23:23:12 -0300

    Refactor RegisterPetDTO to update field names and add validation for pet registration details

--- src/main/java/com/petspot/dto/pet/RegisterPetDTO.java
+++ src/main/java/com/petspot/dto/pet/RegisterPetDTO.java
@@ -9,25 +9,25 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public record RegisterPetDTO(
-        @NotBlank(message = "Nome é obrigatório") String nome,
-        @NotBlank(message = "Espécie é obrigatória") String especie,
-        @NotNull(message = "Gênero é obrigatório") Integer genero,
-        @NotBlank(message = "Raça é obrigatória") String raca,        
-        String peso,
-        @NotNull(message = "Deve específicar se o Animal foi castrado")Boolean castrado,
-        @NotBlank(message = "comportamento é obrigatório")String comportamento,
-        @NotBlank(message = "Porte é obrigatório")String porte,
-        @NotNull(message = "Deve específicar se o animal foi vacinado")Boolean vacinado,
+        @NotBlank(message = "Nome é obrigatório") String name,
+        @NotBlank(message = "Espécie é obrigatória") String species,
+        @NotNull(message = "Gênero é obrigatório") Integer gender,
+        @NotBlank(message = "Raça é obrigatória") String breed,
+        String weight,
+        @NotNull(message = "Deve específicar se o Animal foi castrado") Boolean neutered,
+        @NotBlank(message = "Comportamento é obrigatório") String behavior,
+        @NotBlank(message = "Porte é obrigatório") String size,
+        @NotNull(message = "Deve específicar se o animal foi vacinado") Boolean vaccinated,
         @NotBlank(message = "Data de nascimento é obrigatória")
-        @Pattern(regexp = "\\d{2}/\\d{2}/\\d{4}", message = "Data deve estar no formato dd/MM/yyyy") String dataDeNascimento) {
-    
+        @Pattern(regexp = "\\d{2}/\\d{2}/\\d{4}", message = "Data deve estar no formato dd/MM/yyyy") String birthDate) {
+
     public Date getDate() {
         SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
 
         try {
-            return format.parse(dataDeNascimento);
+            return format.parse(birthDate);
         } catch (ParseException e) {
-            throw new RuntimeException(e);
+            throw new IllegalArgumentException("Invalid date format. Please use dd/MM/yyyy.", e);
         }
     }
 }
