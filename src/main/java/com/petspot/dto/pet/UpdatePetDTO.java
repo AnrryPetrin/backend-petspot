16fc9bd96a9fa0f30b1c2bc5da54621ac264db9d
Anrry Petrin De Araujo <108158140+AnrryPetrin@users.noreply.github.com>
2024-10-16 23:23:20 -0300

    Refactor UpdatePetDTO to update field name for pet name

--- src/main/java/com/petspot/dto/pet/UpdatePetDTO.java
+++ src/main/java/com/petspot/dto/pet/UpdatePetDTO.java
@@ -3,5 +3,5 @@ package com.petspot.dto.pet;
 import jakarta.validation.constraints.NotBlank;
 
 public record UpdatePetDTO(
-        @NotBlank(message = "Pet name cannot be blank") String petName) {
+        @NotBlank(message = "Pet name cannot be blank") String name) {
 }
