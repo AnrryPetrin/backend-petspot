71843806ec364323c29b6fedebd8684894e4da33
Anrry Petrin De Araujo <108158140+AnrryPetrin@users.noreply.github.com>
2024-10-16 23:23:23 -0300

    Refactor UpdatePetWeightDTO to update field name for pet weight

--- src/main/java/com/petspot/dto/pet/UpdatePetWeightDTO.java
+++ src/main/java/com/petspot/dto/pet/UpdatePetWeightDTO.java
@@ -3,5 +3,5 @@ package com.petspot.dto.pet;
 import jakarta.validation.constraints.NotBlank;
 
 public record UpdatePetWeightDTO(
-        @NotBlank(message = "Weight cannot be blank") String peso) {
+        @NotBlank(message = "Weight cannot be blank") String weight) {
 }
