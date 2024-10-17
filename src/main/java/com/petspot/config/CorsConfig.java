d1f8715e609768d07cd6bb2769be93c4246eff1e
Anrry Petrin De Araujo <108158140+AnrryPetrin@users.noreply.github.com>
2024-10-16 23:22:50 -0300

    Refactor CorsConfig class to allow all HTTP methods and headers in CORS configuration

--- src/main/java/com/petspot/config/CorsConfig.java
+++ src/main/java/com/petspot/config/CorsConfig.java
@@ -11,8 +11,12 @@ public class CorsConfig {
     WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
-            public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("https://petspot-frontend.vercel.app/");
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://petspot-frontend.vercel.app/")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
             }
         };
     }
